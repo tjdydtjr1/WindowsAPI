6658dca3c66abf1ea450df6f200c14d9b043a44e
tjdydtjr1 <114229556+tjdydtjr1@users.noreply.github.com>
2023-06-08 23:51:32 +0900

    글자쓰기 이어서

--- WinAPI/WinMain.cpp
+++ WinAPI/WinMain.cpp
@@ -56,8 +56,10 @@ LPTSTR _lpszClass = TEXT("Windows API");
 int danX = 10;
 int danY = 100;
 int Y = 0;
+
 int dan = 1;
 char test[30];
+int name = 0;
 
 RECT rect;
 
@@ -325,40 +327,371 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
     case WM_LBUTTONDOWN:
         // WM_PAINT가 아니기에 GetDC를 사용하여 가져온다.
         hdc = GetDC(hWnd);
-        ++dan;
+    /*    ++dan;
+        danX = 10;
         danY = 85;
-        if (dan == 5)
+        Y = 0;
+        if (dan < 2)
         {
-            danX = 10;
-            Y = 160;
+            dan = 1;
+            return 0;
         }
-        else if (dan == 8)
+        else if (dan > 9)
         {
-            danX = 10;
-            Y = 320;
+            dan = 8;
         }
-        if (0 < dan && dan < 10)
+        
+        for (int i = 2; i <= dan; ++i)
         {
-            for (int i = 1; i < 10; ++i)
+
+            if (i >= 5 && i < 8)
             {
-                sprintf_s(test, "%d x %d = %d", dan, i, dan * i);
-                TextOut(hdc, danX, danY + Y, test, strlen(test));
+                if (i == 5)
+                {
+                    danX = 10;
+                }
+                Y = 160;
+            }
+            else if (i >= 8)
+            {
+                if (i == 8)
+                {
+                    danX = 10;
+                }
+                Y = 320;
+            }
+
+            for (int j = 1; j < 10; ++j)
+            {
+                sprintf_s(test, "%d x %d = %d", i, j, i * j);
+                TextOut(hdc, danX, (danY + Y), test, strlen(test));
                 danY += 15;
             }
             danX += 120;
-            // 사용후 DC 해제
-            ReleaseDC(hWnd, hdc);
+            danY = 85;
+
+     
+        }*/
+        // 이름
+        //++name;
+        //if (name == 1)
+        //{
+        //    // 서
+        //    MoveToEx(hdc, 100, 150, NULL);
+        //    LineTo(hdc, 10, 250);
+
+        //    MoveToEx(hdc, 50, 200, NULL);
+        //    LineTo(hdc, 100, 250);
+
+        //    MoveToEx(hdc, 60, 200, NULL);
+        //    LineTo(hdc, 120, 200);
+
+        //    MoveToEx(hdc, 120, 130, NULL);
+        //    LineTo(hdc, 120, 270);
+        //}
+        //else if (name == 2)
+        //{
+        //    // 용
+        //    MoveToEx(hdc, 200, 130, NULL);
+        //    LineTo(hdc, 201, 130);
+
+
+        //    MoveToEx(hdc, 260, 160, NULL);
+        //    LineTo(hdc, 260, 180);
+
+        //    LineTo(hdc, 250, 190);
+        //    LineTo(hdc, 240, 200);
+        //    LineTo(hdc, 220, 200);
+        //    LineTo(hdc, 200, 180);
+        //    LineTo(hdc, 200, 160);
+        //    LineTo(hdc, 220, 140);
+        //    LineTo(hdc, 240, 140);
+        //    LineTo(hdc, 260, 160);
+
+        //    MoveToEx(hdc, 240, 200, NULL);
+        //    LineTo(hdc, 240, 220);
+        //    MoveToEx(hdc, 220, 200, NULL);
+        //    LineTo(hdc, 220, 220);
+        //    MoveToEx(hdc, 160, 220, NULL);
+        //    LineTo(hdc, 300, 220);
+
+        //    MoveToEx(hdc, 260, 260, NULL);
+        //    LineTo(hdc, 260, 280);
+
+        //    LineTo(hdc, 250, 290);
+        //    LineTo(hdc, 240, 300);
+        //    LineTo(hdc, 220, 300);
+        //    LineTo(hdc, 200, 280);
+        //    LineTo(hdc, 200, 260);
+        //    LineTo(hdc, 220, 240);
+        //    LineTo(hdc, 240, 240);
+        //    LineTo(hdc, 260, 260);
+        //}
+        //else if (name == 3)
+        //{
+        //    // 석
+        //    MoveToEx(hdc, 400, 150, NULL);
+        //    LineTo(hdc, 310, 250);
+
+        //    MoveToEx(hdc, 350, 200, NULL);
+        //    LineTo(hdc, 400, 250);
+
+        //    MoveToEx(hdc, 460, 200, NULL);
+        //    LineTo(hdc, 400, 200);
+
+        //    MoveToEx(hdc, 460, 130, NULL);
+        //    LineTo(hdc, 460, 270);
+
+        //    MoveToEx(hdc, 350, 300, NULL);
+        //    LineTo(hdc, 470, 300);
+        //    LineTo(hdc, 470, 350);
+        //}
+        /*for (int i = 0; i < 5; ++i)
+        {
+            LineTo(hdc, 260 - i, 160);
         }
+        for (int i = 0; i < 5; ++i)
+        {
+            LineTo(hdc, 255 - i, 158);
+        }
+        for (int i = 0; i < 5; ++i)
+        {
+            LineTo(hdc, 250 - i, 156);
+        }
+        for (int i = 0; i < 5; ++i)
+        {
+            LineTo(hdc, 245 - i, 154);
+        }
+        for (int i = 0; i < 5; ++i)
+        {
+            LineTo(hdc, 240 - i, 152);
+        }
+        for (int i = 0; i < 5; ++i)
+        {
+            LineTo(hdc, 235 - i, 150);
+        }*/
+     
+        //Unreal Engine
+        // U
+        MoveToEx(hdc, 100, 100, NULL);
+        LineTo(hdc, 100, 150);
+        LineTo(hdc, 105, 153);
+      
+        for (int i = 0; i < 5; ++i)
+        {
+            LineTo(hdc, 105 + i, 153);
+        }
+        for (int i = 0; i < 5; ++i)
+        {
+            LineTo(hdc, 110 + i, 154);
+        }
+        for (int i = 0; i < 5; ++i)
+        {
+            LineTo(hdc, 115 + i, 153);
+        }
+        for (int i = 0; i < 5; ++i)
+        {
+            LineTo(hdc, 120 + i, 152);
+        }
+        MoveToEx(hdc, 125, 152, NULL);
+        LineTo(hdc, 125, 100);
+      ///////
+        
+        // n
+        MoveToEx(hdc, 140, 150, NULL);
+        LineTo(hdc, 140, 120);
+        LineTo(hdc, 170, 120);
+        LineTo(hdc, 170, 150);
+        MoveToEx(hdc, 180, 120, NULL);
+        LineTo(hdc, 180, 150);
+
+        //r
+        MoveToEx(hdc, 180, 125, NULL);
+       
+        for (int i = 0; i < 3; ++i)
+        {
+            LineTo(hdc, 180 + i, 125);
+        }
+        for (int i = 0; i < 3; ++i)
+        {
+            LineTo(hdc, 185 + i, 124);
+        }
+        for (int i = 0; i < 3; ++i)
+        {
+            LineTo(hdc, 190 + i, 123);
+        }
+        for (int i = 0; i < 3; ++i)
+        {
+            LineTo(hdc, 195 + i, 124);
+        }
+        for (int i = 0; i < 3; ++i)
+        {
+            LineTo(hdc, 200 + i, 125);
+        }
+        ///
+        // e
+        MoveToEx(hdc, 210, 125, NULL);
+        for (int i = 0; i < 3; ++i)
+        {
+            LineTo(hdc, 215 + i, 125);
+        }
+        for (int i = 0; i < 3; ++i)
+        {
+            LineTo(hdc, 220 + i, 124);
+        }
+        for (int i = 0; i < 3; ++i)
+        {
+            LineTo(hdc, 225 + i, 125);
+        }
+        for (int i = 0; i < 3; ++i)
+        {
+            LineTo(hdc, 230 + i, 126);
+        }
+        for (int i = 0; i < 3; ++i)
+        {
+            LineTo(hdc, 235 + i, 127);
+        }
+        for (int i = 0; i < 3; ++i)
+        {
+            LineTo(hdc, 240 + i, 130);
+        }
+        for (int i = 0; i < 3; ++i)
+        {
+            LineTo(hdc, 240 + i, 132);
+        }
+        for (int i = 0; i < 3; ++i)
+        {
+            LineTo(hdc, 240 + i, 130 + i);
+        }
+        LineTo(hdc, 240, 140);
+        LineTo(hdc, 210, 140);
+        LineTo(hdc, 210, 125);
+        LineTo(hdc, 210, 150);
+        LineTo(hdc, 215, 155);
+        LineTo(hdc, 235, 155);
+
+        // a
+        MoveToEx(hdc, 280, 140, NULL);
+        LineTo(hdc, 280, 144);
+        LineTo(hdc, 280, 150);
+        LineTo(hdc, 275, 155);
+        LineTo(hdc, 270, 156);
+        LineTo(hdc, 265, 157);
+        LineTo(hdc, 270, 157);
+        LineTo(hdc, 265, 158);
+        LineTo(hdc, 260, 158);
+        LineTo(hdc, 255, 156);
+        LineTo(hdc, 250, 155);
+        LineTo(hdc, 252, 150);
+        LineTo(hdc, 250, 152);
+        LineTo(hdc, 250, 150);
+        LineTo(hdc, 250, 148);
+        LineTo(hdc, 253, 146);
+        LineTo(hdc, 253, 144);
+        LineTo(hdc, 253, 142);
+        LineTo(hdc, 256, 142);
+        LineTo(hdc, 260, 141);
+        LineTo(hdc, 265, 141);
+        LineTo(hdc, 270, 140);
+        LineTo(hdc, 275, 139);
+        LineTo(hdc, 280, 139);
+        LineTo(hdc, 280, 135);
+        LineTo(hdc, 280, 140);
+        LineTo(hdc, 278, 145);
+        LineTo(hdc, 280, 144);
+        LineTo(hdc, 280, 130);
+        LineTo(hdc, 278, 129);
+        LineTo(hdc, 277, 128);
+        LineTo(hdc, 275, 126);
+        LineTo(hdc, 270, 124);
+        LineTo(hdc, 265, 123);
+        LineTo(hdc, 260, 123);
+
+
+        // l
+        MoveToEx(hdc, 300, 123, NULL);
+        LineTo(hdc, 300, 160);
+
+        // E
+        MoveToEx(hdc, 320, 120, NULL);
+        LineTo(hdc, 350, 120);
+        MoveToEx(hdc, 320, 120, NULL);
+        LineTo(hdc, 320, 140);
+        LineTo(hdc, 350, 140);
+        MoveToEx(hdc, 320, 140, NULL);
+        LineTo(hdc, 320, 160);
+        MoveToEx(hdc, 320, 160, NULL);
+        LineTo(hdc, 350, 160);
+
+        // n
+        MoveToEx(hdc, 360, 160, NULL);
+        LineTo(hdc, 360, 125);
+        LineTo(hdc, 365, 120);
+        LineTo(hdc, 370, 119);
+        LineTo(hdc, 375, 118);
+        LineTo(hdc, 380, 119);
+        LineTo(hdc, 385, 120);
+        LineTo(hdc, 390, 122);
+        LineTo(hdc, 390, 160);
+
+        // g
+        MoveToEx(hdc, 415, 120, NULL);
+        LineTo(hdc, 430, 120);
+        MoveToEx(hdc, 415, 120, NULL);
+        LineTo(hdc, 410, 122);
+        LineTo(hdc, 405, 124);
+        LineTo(hdc, 405, 130);
+        LineTo(hdc, 410, 134);
+        LineTo(hdc, 415, 135);
+        LineTo(hdc, 420, 136);
+        LineTo(hdc, 425, 137);
+        LineTo(hdc, 435, 137);
+        LineTo(hdc, 435, 145);
+        LineTo(hdc, 430, 146);
+        LineTo(hdc, 430, 148);
+        LineTo(hdc, 420, 150);
+        LineTo(hdc, 415, 150);
+        LineTo(hdc, 410, 150);
+        MoveToEx(hdc, 415, 120, NULL);
+        LineTo(hdc, 430, 120);
+        LineTo(hdc, 435, 125);
+        LineTo(hdc, 435, 128);
+        LineTo(hdc, 435, 130);
+        LineTo(hdc, 435, 135);
+        LineTo(hdc, 435, 150);
+
+        // i
+        MoveToEx(hdc, 450, 125, NULL);
+        LineTo(hdc, 450, 160);
+        MoveToEx(hdc, 450, 120, NULL);
+
+        for (int i = 0; i < 10; ++i)
+        {
+            LineTo(hdc, 445 + i, 120);
+        }
+
+        // n
+
+        //
+        // 사용후 DC 해제
+        ReleaseDC(hWnd, hdc);
         break;
 
     case WM_RBUTTONDOWN:
-        if (0 < dan)
-        {
-            dan -= 2;
-        }
-        rect = { danX - 120, danY - 85, danX , danY };
-        InvalidateRect(hWnd, &rect, true);
-        
+        /*dan -= 2;
+        InvalidateRect(hWnd, NULL, true);
+        PostMessage(hWnd, WM_PAINT, 0, 0);
+        PostMessage(hWnd, WM_LBUTTONDOWN, 0, 0);*/
+
+        // 이름
+        /*name -= 2;
+        InvalidateRect(hWnd, NULL, true);
+        PostMessage(hWnd, WM_PAINT, 0, 0);
+        PostMessage(hWnd, WM_LBUTTONDOWN, 0, 0);*/
+
+
+
+
         break;
 
     case WM_KEYDOWN:
